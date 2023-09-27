5d8d7403b743bb8bb38730e60444e414ab1ddd8f
modengann <99417989+modengann@users.noreply.github.com>
2023-09-26 20:28:12 -0400

    init

---
+++ src/App.java
@@ -0,0 +1,14 @@
+/** Write a Java program that calculates the sum of all odd numbers entered by the user 
+ *  until the user enters -1. Even numbers should be ignored. Negative numbers should also 
+ *  be ignored (except -1 which will exit the program). After -1 is entered, display the total sum.
+ * 
+ * 
+ */
+
+import java.util.*;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Scanner scanner = new Scanner(System.in);
+    }
+}
